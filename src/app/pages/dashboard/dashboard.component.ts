a9adb5aed45c98a4cb4962bf83e5accaa0133fd7
Nthabi <mputle15@gmail.com>
2019-02-26 07:41:41 +0200

    new changes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -50,45 +50,51 @@ export class DashboardComponent implements OnInit {
   constructor(private stats: statsService) { }
 
   ngOnInit() {
+    console.log(this.temp);
+    
     this.getAllFarms().then(() => {
       //this.getFarmGeoStats()
-      var ctx = document.getElementById("ratings");
-    this.chart = new Chart("ratings", {
-        type: 'doughnut',
-        data: {
-          labels: this.farmName,
-          datasets: [{
-            label: 'my data',
-            data: this.temp,
-            backgroundColor: [
-              'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-              'rgba(255,99,132,1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: false
+
+      setTimeout(() => {
+        var ctx = document.getElementById("ratings");
+        this.chart = new Chart("ratings", {
+            type: 'doughnut',
+            data: {
+              labels: this.farmName,
+              datasets: [{
+                label: 'my data',
+                data: this.farmRating,
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+              }]
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
               }
-            }]
-          }
-        }
-      });
+            }
+          });
+      }, 3000);
+     
 
     })
 
@@ -107,6 +113,8 @@ export class DashboardComponent implements OnInit {
 
           this.farmRating.push(rate)
           this.farmName.push(name)
+          console.log(this.farmRating);
+          
 
 
         }
